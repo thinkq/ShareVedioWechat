01b45c338aa28681ba7d959063f40519c7f685d6
zhaoqing <985344727@qq.com>
2017-05-26 16:25:56 +0800

    Initial Commit

---
+++ ShareVedioWechat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareVedioWechat
+//
+//  Created by 赵清 on 2017/5/26.
+//  Copyright © 2017年 赵清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShareVedioWechat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ShareVedioWechat
+//
+//  Created by 赵清 on 2017/5/26.
+//  Copyright © 2017年 赵清. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
